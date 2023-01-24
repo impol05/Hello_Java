e00e49677385594787ae3e80a51c09184cddc639
impol05 <122793944+impol05@users.noreply.github.com>
2023-01-24 13:13:02 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
-// Your First Program
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
+public class Hello {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
 }
